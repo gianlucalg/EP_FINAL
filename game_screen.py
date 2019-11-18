4eb68d97f31831e8ef39baafc185d67769735de7
Amanda Carmo <amanda.rosa.ismart@gmail.com>
2019-11-18 17:22:24 -0300

    update main e game screen

--- game_screen.py
+++ game_screen.py
@@ -1,12 +1,12 @@
 import pygame
 import random
 from os import path
-
 from config import img_dir, snd_dir, fnt_dir, WIDTH, HEIGHT, BLACK, YELLOW, RED, FPS, QUIT
+pygame.init()
 
 
 # Carrega todos os assets uma vez só.
-def load_assets(img_dir, snd_dir):
+def load_assets(img_dir, snd_dir,fnt_dir = ''):
     assets = {}
     assets["player_img"] = pygame.image.load(path.join(img_dir, "player.png")).convert()
     assets["arrow_green_up_img"] = pygame.image.load(path.join(img_dir, "arrow_green_up.png")).convert()
@@ -17,12 +17,12 @@ def load_assets(img_dir, snd_dir):
     assets["arrow_red_down_img"] = pygame.image.load(path.join(img_dir, "arrow_red_down.png")).convert()
     assets["arrow_red_left_img"] = pygame.image.load(path.join(img_dir, "arrow_red_left.png")).convert()
     assets["arrow_red_right_img"] = pygame.image.load(path.join(img_dir, "arrow_red_right.png")).convert()
-    assets["background"] = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
-    assets["jump_sound"] = pygame.mixer.Sound(path.join(snd_dir, 'jump_sound.wav'))
+    assets["background"] = pygame.image.load(path.join(img_dir, 'background.png')).convert()
+    #assets["jump_sound"] = pygame.mixer.Sound(path.join(snd_dir, 'jump_sound.wav')).convert()
 
     return assets
 
-assets = load_assets(img_dir, snd_dir)
+
 
 # Classe Jogador que representa o personagem
 class Player(pygame.sprite.Sprite):
@@ -47,163 +47,24 @@ class Player(pygame.sprite.Sprite):
         # Velocidade da nave
         self.speedx = 0
         
-    
-    # Metodo que atualiza a posição do personagem
-    def update(self):
-        self.rect.x += self.speedx
-        
-        # Mantem dentro da tela
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
+
                     
-        
             
 class Arrow(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, arrow_img):
+    def __init__(self, img):
         pygame.sprite.Sprite.__init__(self)
 
         #Escolhe imagem do ícone da seta que vai aparecer.
-        self.img_lib = []
-        self.img_lib.append(assets["arrow_green_up_img"])
-        self.img_lib.append(assets["arrow_green_down_img"])
-        self.img_lib.append(assets["w_green_left_img"])
-        self.img_lib.append(assets["w_green_right_img"])
-        self.img_lib.append(assets["w_red_up_img"])
-        self.img_lib.append(assets["w_red_down_img"])
-        self.img_lib.append(assets["w_red_left_img"])
-        self.img_lib.append(assets["w_red_right_img"])
-
-        self.change_arrow()
-
-    def change_arrow(self):
-        arrow = random.randint(len(self.img_lib))
-        self.image = pygame.transform.scale(self.img_lib[arrow], (50, 38))
+        self.image = pygame.transform.scale(img, (50, 38))
 
         self.rect = self.image.get_rect()
         
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
-
-    def update(self):
-        self.rect.x += self.speedx
-        
-        # Mantem dentro da tela
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
-
         
 
-
-
-def game_screen(screen):
-    assets = load_assets(img_dir, snd_dir, fnt_dir)
-
-    # Variável para o ajuste de velocidade
-    clock = pygame.time.Clock()
-
-    # Carrega o fundo do jogo
-    background = assets["background"]
-    background_rect = background.get_rect()
-
-    # Carrega os sons do jogo
-    pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
-    pygame.mixer.music.set_volume(0.4)
-    jump_sound = assets["jump_sound"]
-
-    # Cria o personagem. O construtor será chamado automaticamente.
-    player = Player(assets["player_img"])
-
-    # Carrega a fonte para desenhar o score.
-    score_font = assets["score_font"]
-'''
-    # Loop principal.
-    pygame.mixer.music.play(loops=-1)
-
-    score = 0
-
-    PLAYING = 0
-    ERROR = 1
-    DONE = 2
-
-    state = PLAYING
-
-    while state != DONE:
-        
-        # Ajusta a velocidade do jogo.
-        clock.tick(FPS)
-        
-        if state == PLAYING:
+      
 
 
 
-
-            # Processa os eventos (mouse, teclado, botão, etc).
-            for event in pygame.event.get():
-                
-                # Verifica se foi fechado.
-                if event.type == pygame.QUIT:
-                    state = ERROR
-                
-                # Verifica se apertou alguma tecla.
-                if event.type == pygame.KEYDOWN:
-                    # Verifica se a tecla apertada corresponde à seta mostrada na tela.
-
-                    if event.key == pygame.K_LEFT and arrow == img_lib["w_green_left_img"]
-
-
-                    event.key == pygame.K_RIGHT
-
-
-                        
-                # Verifica se soltou alguma tecla.
-                if event.type == pygame.KEYUP:
-                    # Dependendo da tecla, altera a velocidade.
-                    if event.key == pygame.K_LEFT:
-                        player.speedx = 0
-                    if event.key == pygame.K_RIGHT:
-                        player.speedx = 0
-                    
-        # Depois de processar os eventos.
-        # Atualiza a acao de cada sprite.
-        all_sprites.update() 
-
-        TIME += 1     
-
-
-
-        elif state == EXPLODING:
-            now = pygame.time.get_ticks()
-            if now - explosion_tick > explosion_duration:
-                if lives == 0:
-                    state = DONE
-                else:
-                    state = PLAYING
-                    player = Player(assets["player_img"])
-                    all_sprites.add(player)
-
-        # A cada loop, redesenha o fundo e os sprites
-        screen.fill(BLACK)
-        screen.blit(background, background_rect)
-        all_sprites.draw(screen)
-
-        # Desenha o score
-        text_surface = score_font.render("{:08d}".format(score), True, YELLOW)
-        text_rect = text_surface.get_rect()
-        text_rect.midtop = (WIDTH / 2,  10)
-        screen.blit(text_surface, text_rect)
-
-        # Desenha as vidas
-        text_surface = score_font.render(chr(9829) * lives, True, RED)
-        text_rect = text_surface.get_rect()
-        text_rect.bottomleft = (10, HEIGHT - 10)
-        screen.blit(text_surface, text_rect)
-        
-        # Depois de desenhar tudo, inverte o display.
-        pygame.display.flip()
-
-    return QUIT
--- main_game.py
+++ main_game.py
@@ -3,7 +3,16 @@ from pygame.locals import *
 import sys
 import os
 from os import path
-from config import img_dir, snd_dir, fnt_dir, WIDTH, HEIGHT, FPS, QUIT
+from config import img_dir, snd_dir, fnt_dir, WIDTH, HEIGHT, FPS, QUIT, BLACK
+from game_screen import load_assets
+from game_screen import Arrow, Player
+import random
+
+
+pygame.display.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.mixer.init()
+
 
 #Fazer o background mexer
 def events():
@@ -12,27 +21,9 @@ def events():
             pygame.quit()
             sys.exit()
             
-            
-            
-def load_assets(img_dir, snd_dir):
-    assets = {}
-    assets["player_img"] = pygame.image.load(path.join(img_dir, "player.png")).convert()
-    assets["arrow_green_up_img"] = pygame.image.load(path.join(img_dir, "arrow_green_up.png")).convert()
-    assets["arrow_green_down_img"] = pygame.image.load(path.join(img_dir, "arrow_green_down.png")).convert()
-    assets["arrow_green_left_img"] = pygame.image.load(path.join(img_dir, "arrow_green_left.png")).convert()
-    assets["arrow_green_right_img"] = pygame.image.load(path.join(img_dir, "arrow_green_right.png")).convert()
-    assets["arrow_red_up_img"] = pygame.image.load(path.join(img_dir, "arrow_red_up.png")).convert()
-    assets["arrow_red_down_img"] = pygame.image.load(path.join(img_dir, "arrow_red_down.png")).convert()
-    assets["arrow_red_left_img"] = pygame.image.load(path.join(img_dir, "arrow_red_left.png")).convert()
-    assets["arrow_red_right_img"] = pygame.image.load(path.join(img_dir, "arrow_red_right.png")).convert()
-    assets["background_img"] = pygame.image.load(path.join(img_dir,"background.png")).convert()
-    
-    return assets
+        
 
-assets = load_assets(img_dir, snd_dir)
-
-arrows_list = ["arrow_green_up_img","arrow_green_down_img","arrow_green_left_img","arrow_green_right_img","arrow_red_up_img","arrow_red_down_img","arrow_red_left_img","arrow_red_right
-_img"]
+arrows_list = ["arrow_green_up_img","arrow_green_down_img","arrow_green_left_img","arrow_green_right_img","arrow_red_up_img","arrow_red_down_img","arrow_red_left_img","arrow_red_right_img"]
 
 
 '''        
@@ -80,19 +71,10 @@ def game_screen(screen):
     background = assets["background"]
     background_rect = background.get_rect()
 
-    # Carrega os sons do jogo
-#   pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
-#   pygame.mixer.music.set_volume(0.4)
-    #jump_sound = assets["jump_sound"]
-
     # Cria o personagem. O construtor será chamado automaticamente.
     player = Player(assets["player_img"])
 
-
-    # Loop principal.
-    #pygame.mixer.music.play(loops=-1)
-
-    #score = 0
+    directions = ['arrow_red_up_img', 'arrow_red_down_img', 'arrow_red_left_img', 'arrow_red_right_img', 'arrow_green_up_img', 'arrow_green_down_img', 'arrow_green_left_img', 'arrow_green_right_img']
 
     PLAYING = 0
     ERROR = 1
@@ -100,8 +82,12 @@ def game_screen(screen):
     TIME = 0
 
     state = PLAYING
+    
+    all_arrows = pygame.sprite.Group()
 
     while state != DONE:
+        
+        print(TIME)
         
         # Ajusta a velocidade do jogo.
         clock.tick(FPS)
@@ -109,78 +95,60 @@ def game_screen(screen):
         if state == PLAYING:
             # Processa os eventos (mouse, teclado, botão, etc).
             
-          
+            # a cada segundo, cria uma arrow.
             if TIME % 1*FPS == 0:
-                arrow = Arrow(pygame.sprite.Sprite)           
-            
-            
-            for event in pygame.event.get():
+                                
+                img = assets[random.choice(directions)]               
                 
-                # Verifica se foi fechado.
-                if event.type == pygame.QUIT:
-                    state = ERROR
+                arrow = Arrow(img)  
+                all_arrows.add(arrow)
                 
+                TIME = 0
                 
-            
-                
-                # Verifica se apertou alguma tecla.
-                if event.type == pygame.KEYDOWN:
-                    # Verifica se a tecla apertada corresponde à seta mostrada na tela.
-
-                    if event.key == pygame.K_LEFT and arrow == 
-
-
-                    event.key == pygame.K_RIGHT
-                    
-                    
-            
-
-
-                        
-                    
-        # Depois de processar os eventos.
-        # Atualiza a acao de cada sprite.
-        all_sprites.update()     
-
-
-                # No lugar do meteoro antigo, adicionar uma explosão.
-                explosao = Explosion(hit.rect.center, assets["explosion_anim"])
-                all_sprites.add(explosao)
-
-                # Ganhou pontos!
-                score += 100
-            
-            # Verifica se houve colisão entre nave e me
-
-            
-        elif state == EXPLODING:
-            now = pygame.time.get_ticks()
-            if now - explosion_tick > explosion_duration:
-                if lives == 0:
-                    state = DONE
-                else:
-                    state = PLAYING
-                    player = Player(assets["player_img"])
-                    all_sprites.add(player)
-
-        # A cada loop, redesenha o fundo e os sprites
+        TIME+=1
         screen.fill(BLACK)
         screen.blit(background, background_rect)
-        all_sprites.draw(screen)
 
-        # Desenha o score
-        text_surface = score_font.render("{:08d}".format(score), True, YELLOW)
-        text_rect = text_surface.get_rect()
-        text_rect.midtop = (WIDTH / 2,  10)
-        screen.blit(text_surface, text_rect)
-
-        # Desenha as vidas
-        text_surface = score_font.render(chr(9829) * lives, True, RED)
-        text_rect = text_surface.get_rect()
-        text_rect.bottomleft = (10, HEIGHT - 10)
-        screen.blit(text_surface, text_rect)
-        
-        # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
-
-    return QUIT
+        
+        
+            
+      #all_arrows.draw(screen)
+        
+game_screen(screen)
+            
+#            for event in pygame.event.get():
+#                
+#                # Verifica se foi fechado.
+#                if event.type == pygame.QUIT:
+#                    state = ERROR
+#                
+#                
+#            
+#                
+#                # Verifica se apertou alguma tecla.
+#                if event.type == pygame.KEYDOWN:
+#                    
+#
+#                
+#
+#        # A cada loop, redesenha o fundo e os sprites
+#        screen.fill(BLACK)
+#        screen.blit(background, background_rect)
+#    
+#        # Desenha o score
+#        text_surface = score_font.render("{:08d}".format(score), True, YELLOW)
+#        text_rect = text_surface.get_rect()
+#        text_rect.midtop = (WIDTH / 2,  10)
+#        screen.blit(text_surface, text_rect)
+#    
+#        # Desenha as vidas
+#        text_surface = score_font.render(chr(9829) * lives, True, RED)
+#        text_rect = text_surface.get_rect()
+#        text_rect.bottomleft = (10, HEIGHT - 10)
+#        screen.blit(text_surface, text_rect)
+#        
+#        # Depois de desenhar tudo, inverte o display.
+#        pygame.display.flip()
+#    
+#    return QUIT
